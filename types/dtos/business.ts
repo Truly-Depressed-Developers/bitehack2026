24162391ae59808b007c339b8ec8f4cdd57d54a1
Krzysztof Wrona <rubikon02@gmail.com>
2026-01-10 19:45:06 +0100

    Add business logo and make description required

--- types/dtos/business.ts
+++ types/dtos/business.ts
@@ -4,12 +4,13 @@ import { TagDTO } from './tag';
 type BusinessDTO = {
   id: string;
   name: string;
-  description?: string;
+  description: string;
   address: string;
   website?: string;
   nip: string;
   pkd: string;
   imageUrl?: string;
+  logoUrl?: string;
   tags: TagDTO[];
   coords: Coordinates;
 };
